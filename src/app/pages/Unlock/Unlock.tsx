5a63c8fc69eb414ef456aae1cb4e62db033ac962
alexbelz123 <belz.alexx@gmail.com>
2023-12-21 16:23:15 +0200

    dev update

--- src/app/pages/Unlock/Unlock.tsx
+++ src/app/pages/Unlock/Unlock.tsx
@@ -15,7 +15,6 @@ import { delay } from 'lib/utils';
 import { Link } from 'lib/woozie';
 
 import { ABTestGroup } from '../../../lib/apis/temple';
-import PageWithImageBg from '../../layouts/PageWithImageBg';
 import { getUserTestingGroupNameActions } from '../../store/ab-testing/actions';
 import { useUserTestingGroupNameSelector } from '../../store/ab-testing/selectors';
 import PageWithImageBg from '../../templates/PageWithImageBg';
