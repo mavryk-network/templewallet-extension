654f6edd2efaa64789cd05af17c9b4d52b75666b
alexbelz123 <belz.alexx@gmail.com>
2024-02-22 01:05:37 +0200

    footer table without dropdown

--- src/app/templates/SwapForm/SwapForm.tsx
+++ src/app/templates/SwapForm/SwapForm.tsx
@@ -8,7 +8,7 @@ import classNames from 'clsx';
 import { Controller, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 
-import { Alert, FormSubmitButton } from 'app/atoms';
+import { Alert, Divider, FormSubmitButton } from 'app/atoms';
 import { useBlockLevel } from 'app/hooks/use-block-level.hook';
 import { useRoute3 } from 'app/hooks/use-route3.hook';
 import { ReactComponent as InfoIcon } from 'app/icons/info.svg';
@@ -444,71 +444,67 @@ export const SwapForm: FC = () => {
         <T id={swapParams.isLoading ? 'searchingTheBestRoute' : 'swap'} />
       </FormSubmitButton>
 
-      <div className="pb-2 mb-2 w-full border-b">
-        <table className={classNames('w-full text-xs text-gray-500', styles['swap-form-table'])}>
-          <tbody>
-            <tr>
-              <td>
-                <span ref={feeInfoIconRef} className="flex w-fit items-center hover:bg-gray-100 text-gray-500">
-                  <T id="routingFee" />
-                  &nbsp;
-                  <InfoIcon className="w-3 h-auto stroke-current" />
-                </span>
-              </td>
-              <td className="text-right text-gray-600">{ROUTING_FEE_PERCENT}%</td>
-            </tr>
-            <tr>
-              <td>
-                <T id="exchangeRate" />
-              </td>
-              <td className="text-right text-gray-600">
-                <SwapExchangeRate
-                  inputAmount={swapParams.data.input !== undefined ? new BigNumber(swapParams.data.input) : undefined}
-                  outputAmount={
-                    swapParams.data.output !== undefined ? new BigNumber(swapParams.data.output) : undefined
-                  }
-                  inputAssetMetadata={inputAssetMetadata}
-                  outputAssetMetadata={outputAssetMetadata}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <T id="slippageTolerance" />
-              </td>
-              <td className="justify-end text-gray-600 flex">
-                <Controller
-                  control={control}
-                  as={SlippageToleranceInput}
-                  error={!!errors.slippageTolerance}
-                  name="slippageTolerance"
-                  rules={{ validate: slippageToleranceInputValidationFn }}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <T id="minimumReceived" />
-              </td>
-              <td className="text-right text-gray-600">
-                <SwapMinimumReceived
-                  minimumReceivedAmount={minimumReceivedAmountAtomic}
-                  outputAssetMetadata={outputAssetMetadata}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <span ref={cashbackInfoIconRef} className="flex w-fit items-center hover:bg-gray-100 text-gray-500">
-                  <T id="swapCashback" />
-                  &nbsp;
-                  <InfoIcon className="w-3 h-auto stroke-current" />
-                </span>
-              </td>
-              <td className="text-right text-gray-600">{SWAP_CASHBACK_PERCENT}%</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="w-full p-4 bg-gray-910 rounded-2xl-plus">
+        <section className={classNames('w-full text-base-plus text-white', 'flex flex-col gap-3')}>
+          <div className="flex items-center justify-between">
+            <div>
+              <span ref={feeInfoIconRef} className="flex w-fit items-center text-white">
+                <T id="routingFee" />
+                &nbsp;
+                <InfoIcon className="w-3 h-auto stroke-2 stroke-white" />
+              </span>
+            </div>
+            <td className="text-right text-white">{ROUTING_FEE_PERCENT}%</td>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>
+              <T id="exchangeRate" />
+            </div>
+            <td className="text-right text-white">
+              <SwapExchangeRate
+                inputAmount={swapParams.data.input !== undefined ? new BigNumber(swapParams.data.input) : undefined}
+                outputAmount={swapParams.data.output !== undefined ? new BigNumber(swapParams.data.output) : undefined}
+                inputAssetMetadata={inputAssetMetadata}
+                outputAssetMetadata={outputAssetMetadata}
+              />
+            </td>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>
+              <T id="slippageTolerance" />
+            </div>
+            <td className="justify-end text-white flex">
+              <Controller
+                control={control}
+                as={SlippageToleranceInput}
+                error={!!errors.slippageTolerance}
+                name="slippageTolerance"
+                rules={{ validate: slippageToleranceInputValidationFn }}
+              />
+            </td>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>
+              <T id="minimumReceived" />
+            </div>
+            <td className="text-right text-white">
+              <SwapMinimumReceived
+                minimumReceivedAmount={minimumReceivedAmountAtomic}
+                outputAssetMetadata={outputAssetMetadata}
+              />
+            </td>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>
+              <span ref={cashbackInfoIconRef} className="flex w-fit items-center text-white">
+                <T id="swapCashback" />
+                &nbsp;
+                <InfoIcon className="w-3 h-auto stroke-2 stroke-white" />
+              </span>
+            </div>
+            <td className="text-right text-white">{SWAP_CASHBACK_PERCENT}%</td>
+          </div>
+        </section>
       </div>
 
       {error && (
@@ -522,16 +518,9 @@ export const SwapForm: FC = () => {
         />
       )}
 
-      <SwapRoute className="mb-6" />
+      <Divider className="my-6" color="bg-divider" />
 
-      <p className="text-center text-gray-700 max-w-xs m-auto">
-        <span className="mr-1">
-          <T id="swapRoute3Description" />
-        </span>
-        <a className="underline" href="https://3route.io" target="_blank" rel="noreferrer">
-          <T id="swapRoute3Link" />
-        </a>
-      </p>
+      <SwapRoute className="mb-4" />
     </form>
   );
 };
