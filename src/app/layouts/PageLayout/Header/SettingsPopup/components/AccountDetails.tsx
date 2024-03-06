577168299d96b5307f27b5c0ce37c13c87b50422
alexbelz123 <belz.alexx@gmail.com>
2024-03-06 02:51:21 +0200

    added Qr

--- src/app/layouts/PageLayout/Header/SettingsPopup/components/AccountDetails.tsx
+++ src/app/layouts/PageLayout/Header/SettingsPopup/components/AccountDetails.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import { QRCode } from 'react-qr-svg';
+
 import { HashChip, Identicon, Name } from 'app/atoms';
 import { PopupModalWithTitle } from 'app/templates/PopupModalWithTitle';
 import { useAccount } from 'lib/temple/front';
@@ -37,6 +39,9 @@ type AccountDetailsProps = {
 export const AccountDetails: FC<AccountDetailsProps> = ({ account }) => {
   return (
     <div className="px-4 pt-2 text-base-plus text-white flex flex-col items-center">
+      <div className="p-6 bg-white rounded-2xl self-center">
+        <QRCode value={account.publicKeyHash} bgColor="#f4f4f4" fgColor="#000000" level="L" style={{ width: 196 }} />
+      </div>
       <div className="mt-6 bg-gray-710 rounded-xl px-4 py-2 w-full self-stretch">
         <HashChip hash={account.publicKeyHash} small={false} trim={false} />
       </div>
